8ef16ed4cf897dc8eaaff72504d500ac438a9ba9
myGod25 <adetunjiemmanuel463@gmail.com>
2026-05-28 16:37:42 +0100

    ad

--- app.py
+++ app.py
@@ -178,6 +178,10 @@ def login():
 <!DOCTYPE html>
 <html>
 <head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Admin Login</title>
 
     <style>
